b7952ecdf4edd87e2d07b36dba2284b48608ea4b
Pedro Antunes <47991446+PedroAntunesCosta@users.noreply.github.com>
2025-02-25 16:05:42 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,8 @@ RENAMED=$7
 # Build from the main source repository
 git clone https://github.com/vtexdocs/docsearch-scraper.git
 
+git checkout PedroAntunesCosta-spider-1
+
 cd docsearch-scraper/
 
 # Install pipenv
